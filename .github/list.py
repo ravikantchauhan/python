51258aa61f97ae9356e18bc1a1b80202077a72e4
Ravikant Chauhan <ravi.aith@gmail.com>
2020-04-12 17:22:41 +0530

    this is only for if statement

---
+++ .github/list.py
@@ -0,0 +1,5 @@
+# list in python
+nums = [25,12,36,95,14]
+# print(nums[-5)
+names = ['ravi','kant','chauhan']
+print(nums,names)
---
+++ if_else.py
@@ -0,0 +1,23 @@
+# Python Conditions and If statements
+# Python supports the usual logical conditions from mathematics:
+#
+# Equals: a == b
+# Not Equals: a != b
+# Less than: a < b
+# Less than or equal to: a <= b
+# Greater than: a > b
+# Greater than or equal to: a >= b
+# These conditions can be used in several ways, most commonly in "if statements" and loops.
+#
+# # An "if statement" is written by using the if keyword.
+# a: int = 330
+# b =2000
+a=int(input("Please enter first number : "))
+b=int(input("Please enter second number : "))
+
+if b > a:
+    print("b is greater than a")
+if b < a:
+    print("a is greater than b")
+if a == b:
+    print("both numbers are equel")
