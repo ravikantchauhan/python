cb8bbf23ec294d277f2a03de29ca0023387d2f7c
Ravikant Chauhan <ravi.aith@gmail.com>
2020-05-08 11:50:48 +0530

    this is for test

--- numpy/1darray_numpy.py
+++ numpy/1darray_numpy.py
@@ -1,4 +1,10 @@
 from numpy import *
 # from array import *
 roll_stu = array([10,20,40,1,3,50])
-print(roll_stu)
+print(roll_stu ,"this is only array")
+print("THIS IS AFTER FOR LOOP")
+roll_stu([3,30])
+n =len(roll_stu)
+print(n, "this is total number for element")
+for i in range(n):
+    print(roll_stu[i])
